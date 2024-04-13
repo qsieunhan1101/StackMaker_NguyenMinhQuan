9e84b0e7fc40954879e7bb4b55c5906d7858d39b
qsieunhan1101 <qsieunhan2k1@gmail.com>
2024-04-13 19:37:22 +0700

    Map Level1,2,3

---
+++ Assets/_Game/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Touch touch;
+    private enum direction{
+        Right , Left , Forward , Backward
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void Move()
+    {
+        
+    }
+    private void AddBrick()
+    {
+
+    }
+    private void ReMoveBrick()
+    {
+
+    }
+    private void ClearBrick()
+    {
+
+    }
+    private void SetAmim()
+    {
+
+    }
+}
